28f927102b36440c09d6787ac5358ddc410c7960
Danish <DanishAhsancs@gmail.com>
2024-05-13 22:11:19 -0400

    basiccally done

--- app/src/main/java/com/example/wellness4everyone/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/wellness4everyone/MyFirebaseMessagingService.java
@@ -26,7 +26,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String title, String messageBody) {
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, NotificationActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
 
